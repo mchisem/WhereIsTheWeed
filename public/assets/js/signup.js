d96403335f61c0202c3636e078063d9539975c65
Maya Chisem <chisem.maya@gmail.com>
2020-10-10 13:47:34 -0700

    the server isn't running- cryingggg

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,32 +1,46 @@
 $(document).ready(() => {
     // Getting references to our form and input
-    const signUpForm = $("form.signup");
-    const emailInput = $("input#email-input");
+    // const signUpForm = $("form.signup");
+    const emailInput = $("input#email");
     const passwordInput = $("input#password-input");
-  
+    const nameInput = $("input#name");
+    const userNameInput = $("input#user-name");
+    const signUp = $("#sign-up")
+
     // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", event => {
+    signUp.on("click", event => {
       event.preventDefault();
       const userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
-      };
+        email: emailInput.val(),
+        password: passwordInput.val(),
+        name: nameInput.val(),
+        userName: userNameInput.val(),
+     };
   
-      if (!userData.email || !userData.password) {
+      console.log(userData.name);
+      console.log(userData.userName);
+      console.log(userData.email);
+
+      if (!userData.email || !userData.password || !userData.name || !userData.userName) {
         return;
       }
       // If we have an email and password, run the signUpUser function
-      signUpUser(userData.email, userData.password);
+      signUpUser(userData.email, userData.password, userData.name, userData.userName);
       emailInput.val("");
       passwordInput.val("");
+      nameInput.val("");
+      userNameInput.val("");
     });
   
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function signUpUser(email, password) {
+    function signUpUser(email, password, name, userName, age) {
       $.post("/api/signup", {
         email: email,
-        password: password
+        password: password,
+        name: name,
+        userName: userName,
+        age: age
       })
         .then(() => {
           window.location.replace("/members");
